de6acf6503373b29c60c7f1bd5bd76303d284330
Pietro <pietro.tambu2003@gmail.com>
2021-01-27 21:08:28 +0100

    commit

--- src/components/ComponentPollution.vue
+++ src/components/ComponentPollution.vue
@@ -166,7 +166,7 @@ export default {
   border: 2px black solid;
   border-radius: 15px;
   box-shadow: 0 0 10px #000;
-  background-color: rgba(255, 255, 255, 0.3);
+  background-color: rgba(255, 255, 255, 0.5);
 }
 
 input {
